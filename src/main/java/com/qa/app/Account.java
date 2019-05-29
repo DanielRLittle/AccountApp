01f9b32b775bb55df8eef562be8a14b23b16c824
DanielRLittle <daniel.r.little.1994@gmail.com>
2019-05-29 15:27:47 +0100

    Pushing new branch

--- src/main/java/com/qa/app/Account.java
+++ src/main/java/com/qa/app/Account.java
@@ -1,6 +1,13 @@
 package com.qa.app;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
+	@Id
+	@GeneratedValue
 	private int id;
 	private String firstName;
 	private String lastName;
